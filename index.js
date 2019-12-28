b94a2d7d4e9997e5c5e9f28ddd00d4453138e66f
Jérémy Halin <jeremy.halin@gmail.com>
2019-12-28 08:52:21 +0100

    feat: construct object per log line

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const readInterface = readline.createInterface({
     // output: process.stdout,
     console: false
 });
+
+let logs = [];
+
 readInterface.on('line', function(line) {
     const result = line.match(/^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+)\s?(\S+)?\s?(\S+)?" (\d{3}|-) (\d+|-)\s?"?([^"]*)"?\s?"?([^"]*)?"?$/);
+    let urlParts = result[6].split('/');
+    logs.push({
+        section: urlParts[1],
+        url: result[6],
+        code: result[8]
+    });
+});
+
+readInterface.on('close', function() {
+    console.log('end of file', logs);
 });
