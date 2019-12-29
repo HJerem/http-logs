82120bdf6a5bedf6b7613b28355a5446ca08129a
Jérémy Halin <jeremy.halin@gmail.com>
2019-12-29 19:14:48 +0100

    feat(writer): add random data to fake log

--- writer.js
+++ writer.js
@@ -1,9 +1,25 @@
-var fs = require('fs'),
-    lineno=0;
-
-var stream = fs.createWriteStream('test.log', {flags:'a'});
+const fs = require('fs');
+const stream = fs.createWriteStream('test.log', {flags:'a'});
+var HttpStatus = require('http-status-codes');
+const someHttpStatusCodes = [
+    HttpStatus.OK,
+    HttpStatus.NOT_FOUND,
+    HttpStatus.UNAUTHORIZED,
+    HttpStatus.BAD_REQUEST,
+    HttpStatus.MOVED_PERMANENTLY
+];
+const somePages = [
+    '/contact/',
+    '/about/', 
+    '/api/v1/ressources',
+    '/'
+];
 
 stream.on('open', function() {
-    console.log('Stream opened, will start writing in 2 secs');
-    setInterval(function() { stream.write('127.0.0.1 - - [28/Jul/2006:10:27:32 -0300] "GET /hidden/ HTTP/1.0" 404 7218'+'\n'); }, 2000);
+    console.log('Stream opened, will start writing in 2 secs every 2 seconds');
+    setInterval(function() {
+        const randomHttpCode = someHttpStatusCodes[Math.floor(Math.random()*someHttpStatusCodes.length)];
+        const randomPage = somePages[Math.floor(Math.random()*somePages.length)];
+        stream.write(`127.0.0.1 - - [28/Jul/2006:10:27:32 -0300] "GET ${randomPage} HTTP/1.0" ${randomHttpCode} 7218`+'\n'); 
+    }, 2000);
 });
