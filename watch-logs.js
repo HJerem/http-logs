23b8b9bb4b7cf1e84720ede09d0d1aecaae4773d
Jérémy Halin <jeremy.halin@gmail.com>
2019-12-31 11:14:41 +0100

    feat(read): check if file exists
    And improve realtime logs of program

--- watch-logs.js
+++ watch-logs.js
@@ -5,16 +5,26 @@ const args = require('yargs').argv;
 const fs = require('fs');
 const readline = require('readline');
 
-const filePath = args.path || 'test.log';
+const filePath = args.path || '/tmp/access.log';
 const time = args.time || 10000;
 
+const seconds = time / 1000;
 let currentPosition = 0;
 
-fs.stat(filePath, {}, function(err, stats) {
-    if(err) throw err;
-    currentPosition = stats.size;
-    processLogs();
-})
+try {
+    if (fs.existsSync(filePath)) {
+        fs.stat(filePath, {}, function (err, stats) {
+            if (err) throw err;
+            currentPosition = stats.size;
+            console.log(`Waiting ${seconds} seconds before starting`);
+            setTimeout(processLogs, time);
+        })
+    } else {
+        console.warn(`There is no file existing at provided path ${filePath}`);
+    }
+} catch (err) {
+    console.warn(err);
+}
 
 // open file
 // set position to last line
@@ -22,42 +32,44 @@ fs.stat(filePath, {}, function(err, stats) {
 // after these 10 seconds, close file
 // and do it again
 function processLogs() {
+    const start = new Date();
+    const end = new Date(start.getTime() + time);
     let logs = {};
     const stream = fs.createReadStream(filePath, {
         encoding: 'utf-8',
         flags: 'r',
         start: currentPosition
     });
-    stream.on('data', function(chunk) {
+    stream.on('data', function (chunk) {
         currentPosition += chunk.length;
         const lines = chunk.toString().split("\n");
 
-        if(lines) {
-            lines.forEach(function(line) {
-                if(line !== '') {
+        if (lines) {
+            lines.forEach(function (line) {
+                if (line !== '') {
                     const result = line.match(/^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+)\s?(\S+)?\s?(\S+)?" (\d{3}|-) (\d+|-)\s?"?([^"]*)"?\s?"?([^"]*)?"?$/);
-                    if(result) {
+                    if (result) {
                         let urlParts = result[6].split('/');
                         const section = urlParts[1];
                         const code = result[8];
-                    
+
                         // check if section has already been created
-                        if(typeof logs[section] === 'undefined') {
+                        if (typeof logs[section] === 'undefined') {
                             logs[section] = {
                                 visits: 0
                             };
                         }
-                    
+
                         logs[section].visits += 1;
-                    
-                        if(typeof logs[section].codes === 'undefined') {
+
+                        if (typeof logs[section].codes === 'undefined') {
                             logs[section].codes = {};
                         }
-                    
-                        if(typeof logs[section].codes[code] === 'undefined') {
+
+                        if (typeof logs[section].codes[code] === 'undefined') {
                             logs[section].codes[code] = 0;
                         }
-                    
+
                         logs[section].codes[code] += 1;
                     } else {
                         console.warn('Skipping wrongly formatted line: ', line);
@@ -67,55 +79,54 @@ function processLogs() {
         }
         // console.log(chunk.toString().split("\n"));
     })
-    .on('close', function() {
-        let sortable = [];
+        .on('close', function () {
+            let sortable = [];
 
-        for(const [key, value] of Object.entries(logs)) {
-            sortable.push([key, value.visits]);
-        }
+            for (const [key, value] of Object.entries(logs)) {
+                sortable.push([key, value.visits]);
+            }
 
-        sortable.sort(function(a, b) {
-            return b[1] - a[1];
-        });
-        // sortable = sortable.slice(0, 10);
+            sortable.sort(function (a, b) {
+                return b[1] - a[1];
+            });
+            // sortable = sortable.slice(0, 10);
 
-        const stats = {};
+            const stats = {};
 
-        // console.log(sortable);
-        sortable.forEach(function(item) {
-            // calculate error rates
-            let errorRate = 0;
-            let redirectionRate = 0;
-            let total = logs[item[0]].visits;
-            let errors = 0;
-            let redirections = 0;
-            for(const [code, number] of Object.entries(logs[item[0]].codes)) {
-                if(code.substr(0, 1) === '4') {
-                    errors += number;
-                } else if(code.substr(0, 1) === '3') {
-                    redirections += number;
+            // console.log(sortable);
+            sortable.forEach(function (item) {
+                // calculate error rates
+                let errorRate = 0;
+                let redirectionRate = 0;
+                let total = logs[item[0]].visits;
+                let errors = 0;
+                let redirections = 0;
+                for (const [code, number] of Object.entries(logs[item[0]].codes)) {
+                    if (code.substr(0, 1) === '4') {
+                        errors += number;
+                    } else if (code.substr(0, 1) === '3') {
+                        redirections += number;
+                    }
                 }
+
+                errorRate = (errors / total) * 100;
+                redirectionRate = (redirections / total) * 100;
+
+                stats[item[0]] = {
+                    visits: item[1],
+                    error_rates: errorRate,
+                    redirection_rates: redirectionRate
+                }
+            });
+
+            if (stats) {
+                const now = new Date();
+                console.info(`Here is what happened from ${start} to ${end} for log file ${filePath} (last ${seconds} seconds)`);
+                console.table(stats);
+            } else {
+                console.info('No stats to display');
             }
-
-            errorRate = (errors/total)*100;
-            redirectionRate = (redirections/total)*100;
-
-            stats[item[0]] = {
-                visits: item[1],
-                error_rates: errorRate,
-                redirection_rates: redirectionRate
-            }
-        });
-
-        if(stats) {
-            console.log('--------');
-            console.log(`Stats about access log file: ${filePath}`);
-            console.log('--------');
-            console.table(stats);
-        } else {
-            console.info('No stats to display');
-        }
-    })
+        })
 
     setTimeout(processLogs, time);
 }
